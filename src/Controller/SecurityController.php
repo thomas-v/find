9036a4de1718606dfc0585cb4b8ac6bfe5545222
thomas-v <thomas.vitiello@outlook.fr>
2018-08-22 23:58:37 +0200

    Mise en place des restrictions d'accès des pages + modifs de style

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,16 +9,27 @@ use App\Form\RegistrationType;
 use App\Entity\User;
 
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder, Security $security)
     {
+        // on r�cup�re le user connect�
+        $user = $security->getUser();
+        
+        //si il existe on bloque l'acc�s � cette route
+        if($user != null){
+            return $this->redirectToRoute('dashboard');
+        }
+        
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
         
@@ -32,25 +43,43 @@ class SecurityController extends AbstractController
             $manager->persist($user);
             $manager->flush();
             
-            return $this->redirectToRoute('security_login');
+            return $this->render('security/registration.html.twig', [
+                'form' => $form->createView(),
+                'title' => 'test',
+                'success_registration' => true
+            ]);
+            
+            //return $this->redirectToRoute('security_login', ['success_registration' => true]);
         }
         
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView(),
-            'title' => 'test'
+            'title' => 'test',
+            'success_registration' => true
         ]);
     }
     
     /**
      * @Route("/connexion", name="security_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils) {
+    public function login(AuthenticationUtils $authenticationUtils, Security $security) {
+        
+        // on récupère le user connecté
+        $user = $security->getUser();
+        
+        //si il existe on bloque l'accés à cette route
+        if($user != null){
+            return $this->redirectToRoute('dashboard');
+        }
+        
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
+        
         return $this->render('security/login.html.twig',[
             'email' => $lastUsername,
             'error' => $error
         ]);
+        
     }
     
     /**
